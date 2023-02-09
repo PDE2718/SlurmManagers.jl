fc1f6b18c3f2d80827ae118b74aea31725244a11
PDE2718 <1095729268@qq.com>
2023-02-10 00:17:56 +0800

    add dep

--- src/SlurmManagers.jl
+++ src/SlurmManagers.jl
@@ -2,6 +2,7 @@ module SlurmManagers
 
 using Distributed
 using Sockets
+using MKL, LinearAlgebra
 export launch, manage, kill, init_worker, connect, SlurmManager, addprocs_slurm, rm_all_procs
 import Distributed: launch, manage, kill, init_worker, connect
 
