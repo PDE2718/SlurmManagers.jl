3461f7779b09f7c569c09053652ab77e450e6b2f
PDE2718 <1095729268@qq.com>
2023-02-10 00:25:04 +0800

    c

--- src/SlurmManagers.jl
+++ src/SlurmManagers.jl
@@ -1,11 +1,8 @@
 module SlurmManagers
 
-using Distributed
-using Sockets
-using MKL, LinearAlgebra
+using Distributed, Sockets, MKL, LinearAlgebra, Hwloc
 export launch, manage, kill, init_worker, connect, SlurmManager, addprocs_slurm, rm_all_procs
 import Distributed: launch, manage, kill, init_worker, connect
-
 include("slurmscript.jl")
 
 end
